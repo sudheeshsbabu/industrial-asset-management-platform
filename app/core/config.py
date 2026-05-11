1f29199dbe935bc9aff2f19ac1b46bd6554ad217
Sudheesh S Babu <sudheesh.s@inapp.com>
2026-05-11 20:10:04 +0530

    Include pydantic settings and logging

---
+++ app/core/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    APP_NAME: str = "AssetOps"
+    APP_HOST: str = "0.0.0.0"
+    APP_PORT: int = 8080
+
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_NAME: str = "assetops"
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "postgres"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
-from aiohttp import web 
+import logging
+from aiohttp import web
+
+from app.core.config import settings
+from app.core.logging import setup_logging
+
+logger = logging.getLogger(__name__)
 
 async def health(request):
     return web.json_response({"status" : "ok"})
@@ -9,4 +15,12 @@ def create_app():
     return app
 
 if __name__ == "__main__":
-    web.run_app(create_app(), host="0.0.0.0", port=8080)
+    setup_logging()
+    logger.info(f"Starting app")
+    app = create_app()
+
+    web.run_app(
+        app=app, 
+        host=settings.APP_HOST, 
+        port=settings.APP_PORT
+    )
