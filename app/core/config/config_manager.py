e36be3b8757648ec578dee7b102ddd8c561da538
Sudheesh S Babu <sudheesh.s@inapp.com>
2026-05-13 13:58:11 +0530

    feat(config): Include loading of local settings

--- app/core/config/config_manager.py
+++ app/core/config/config_manager.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 import time
 
+from app.core.config.config import Settings
+
 from app.repositories.config_repository import (
     fetch_db_configs,
     sync_env_to_db
@@ -10,25 +12,26 @@ from app.repositories.config_repository import (
 logger = logging.getLogger(__name__)
 
 class ConfigManager:
-    def __init__(self, settings):
-        self.settings = settings
+    def __init__(self):
+        self.settings = {}
         self.runtime_config = {}
 
-    async def load(self, app, new_settings=None):
+    async def load(self, app):
         """Refresh the in-memory runtime config from the DB (DB values override env defaults)."""
-        if new_settings:
-            self.settings = new_settings # override the env settings with the new settings
+
+        # Load local settings
+        self.load_local_settings()
 
         async with app["db"].acquire() as conn:
             db_config = await fetch_db_configs(conn)
 
         merged = {
-            **self.settings.model_dump(),
-            **db_config # db config takes presedence over env config
+            **db_config,
+            **self.settings.model_dump(), # load_local_settings() overrides the  db & env settings
         }
 
         self.runtime_config = merged
-        # logger.info(f"Config Refreshed at {time.time()}")
+        logger.info(f"Config Refreshed at {time.time()} with values : {self.settings.model_dump()}")
 
     def get(self, key, default=None):
         return self.runtime_config.get(key, default)
@@ -48,3 +51,9 @@ class ConfigManager:
             logger.info(f"Env->DB Import: inserted/updated keys: {updated_keys}")
         else:
             logger.info("Env->DB Import: All settings already up to date, no changes needed.")
+
+    def load_local_settings(self):
+        """Load local settings from the .env.local file."""
+        self.settings = Settings(
+            _env_file=[".env", ".env.local"]
+        )
