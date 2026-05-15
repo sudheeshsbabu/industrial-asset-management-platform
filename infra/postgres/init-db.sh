38100df9a581369f67c25086cb96285caac511eb
Sudheesh S Babu <sudheesh.s@inapp.com>
2026-05-15 21:06:55 +0530

    feat(liquibase): Convert xml formats to modern yaml format

--- infra/postgres/init-db.sh
+++ infra/postgres/init-db.sh
@@ -3,6 +3,6 @@ set -e
 
 # Create the additional database if it doesn't exist
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-  SELECT 'CREATE DATABASE assetops_lb'
-  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'assetops_lb')\gexec
+  SELECT 'CREATE DATABASE assetops'
+  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'assetops')\gexec
 EOSQL
